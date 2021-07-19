75b7ff1f4f24bb1a4a05283e7b393e90d9455b82
Philipp <pd95@users.noreply.github.com>
2021-07-19 17:25:13 +0200

    Initial commit

---
+++ Sudoku4Me/Sudoku4MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudoku4MeApp.swift
+//  Sudoku4Me
+//
+//  Created by Philipp on 19.07.21.
+//
+
+import SwiftUI
+
+@main
+struct Sudoku4MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
