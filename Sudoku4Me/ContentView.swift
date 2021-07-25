f68273a322d2eaccdeee4f58af2fe2cf81a6b3b6
Philipp <pd95@users.noreply.github.com>
2021-07-25 17:17:35 +0200

    Adding a very basic sudoku grid view to app

--- Sudoku4Me/ContentView.swift
+++ Sudoku4Me/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var game = SudokuGame.example
+
+    let rows: [GridItem] = SudokuGame.positionRange.map({ _ in
+        GridItem(.flexible(minimum: 30, maximum: 44), spacing: 0)
+    })
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Sudoku!")
+                .font(.largeTitle)
+                .padding()
+
+            LazyHGrid(
+                rows: rows,
+                spacing: 0,
+                content: gridCells
+            )
+        }
+    }
+
+    func gridCells() -> some View {
+        ForEach(0..<9) { y in
+            ForEach(0..<9) { x in
+                ZStack {
+                    Rectangle()
+                        .stroke(Color.primary)
+
+                    if let value = game.value(at: (x,y)) {
+                        Text("\(value)")
+                            .font(.title2)
+                    }
+                }
+                .aspectRatio(1, contentMode: .fill)
+            }
+        }
     }
 }
 
