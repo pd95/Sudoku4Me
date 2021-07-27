6a6e892adfd1d92b41e1c66a00cc286241b4d7a8
Philipp <pd95@users.noreply.github.com>
2021-07-27 14:59:55 +0200

    Adding a nearly complete grid example

--- Sudoku4Me/SudokuGame.swift
+++ Sudoku4Me/SudokuGame.swift
@@ -26,6 +26,18 @@ struct SudokuGame {
         7,0,9,1,0,0,8,0,5,
     ])
 
+    static let example2 = try! SudokuGame(initialGrid: [
+        6,3,4,9,1,5,7,2,8,
+        2,7,8,6,4,3,5,1,0,
+        5,9,1,2,7,8,6,4,3,
+        4,5,7,3,6,9,2,8,1,
+        9,8,6,4,2,1,3,5,7,
+        3,1,2,8,5,7,4,9,6,
+        1,2,5,7,8,6,9,3,4,
+        8,6,3,5,9,4,1,7,2,
+        7,4,9,1,3,2,8,6,5,
+    ])
+
     typealias GridPosition = (x: Int, y: Int)
     typealias GridValue = Int?
 
